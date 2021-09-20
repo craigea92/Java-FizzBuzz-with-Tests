126430ee9a436569d9663b44c1f340d9346b8ee8
Craig Eaton <craigea92@gmail.com>
2021-09-20 11:10:51 +0100

    3 = Fizz, 1 Passing Test

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class FizzBuzz {
+    public String generate(Integer upto) {
+        ArrayList<String> newArray = new ArrayList<>();
+        for (int i = 1; i <= upto; i++) {
+            if (i % 3 == 0) {
+                newArray.add("Fizz");
+            } else {
+                newArray.add(String.valueOf(i));
+            }
+        }
+        return String.join(", ", newArray);
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void testGenerateListOfNumbersUpToThree() {
+        FizzBuzz subject = new FizzBuzz();
+        String result = subject.generate(3);
+        assertEquals("1, 2, Fizz", result);
+
+    }
 }
